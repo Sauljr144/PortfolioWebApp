64b332cd8a918a050b97880b8275e0dfb9798ba1
Sauljr144 <saulsuazojr06@gmail.com>
2023-11-09 19:03:12 -0800

    testing

--- portfolio/src/components/HomePage.jsx
+++ portfolio/src/components/HomePage.jsx
@@ -31,7 +31,7 @@ const HomePage = () => {
             width="100%"
             height="350px"
             borderRadius={30}
-            bgImage="/src/assets/homepage/computer.jpg"
+            bgImage="src/assets/homepage/computer.jpg"
             bgPosition="center"
             bgSize="cover"
             cursor='pointer'
