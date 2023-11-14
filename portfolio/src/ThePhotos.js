14606dfd805c17f565d520c60b7e45deac4606a3
Sauljr144 <saulsuazojr06@gmail.com>
2023-11-14 12:34:31 -0800

    phototest5

--- portfolio/src/ThePhotos.js
+++ portfolio/src/ThePhotos.js
@@ -1,22 +1,27 @@
+
+import p1 from "../src/assets/Photography/Bibi&James_0025.jpg"
+import p2 from "../src/assets/Photography/David_Fernanda_1.jpg"
 const myPhotos = [
-  "../src/assets/Photography/Bibi&James_0025.jpg",
-  "../src/assets/Photography/David_Fernanda_1.jpg",
-  "../src/assets/Photography/Bibi&James_0038.jpg",
-  "../src/assets/Photography/David_Fernanda_2.jpg",
-  "../src/assets/Photography/Bibi&James_0032.jpg",
-  "../src/assets/Photography/Bibi&James_0037.jpg",
-  "../src/assets/Photography/Bibi&James_0047.jpg",
-  "../src/assets/Photography/David_Fernanda_3.jpg",
-  "../src/assets/Photography/Denise&Adrian_0007.jpg",
-  "../src/assets/Photography/Bibi&James_0057.jpg",
-  "../src/assets/Photography/David_Fernanda_4.jpg",
-  "../src/assets/Photography/Denise&Adrian_0011.jpg",
-  "../src/assets/Photography/David_Fernanda_9.jpg",
-  "../src/assets/Photography/Denise&Adrian_0028.jpg",
-  "../src/assets/Photography/David_Fernanda_6.jpg",
-  "../src/assets/Photography/David_Fernanda_10.jpg",
-  "../src/assets/Photography/Denise&Adrian_0026.jpg",
-  "../src/assets/Photography/David_Fernanda_8.jpg",
+    p1,
+    p2
+//   "../src/assets/Photography/Bibi&James_0025.jpg",
+//   "../src/assets/Photography/David_Fernanda_1.jpg",
+//   "../src/assets/Photography/Bibi&James_0038.jpg",
+//   "../src/assets/Photography/David_Fernanda_2.jpg",
+//   "../src/assets/Photography/Bibi&James_0032.jpg",
+//   "../src/assets/Photography/Bibi&James_0037.jpg",
+//   "../src/assets/Photography/Bibi&James_0047.jpg",
+//   "../src/assets/Photography/David_Fernanda_3.jpg",
+//   "../src/assets/Photography/Denise&Adrian_0007.jpg",
+//   "../src/assets/Photography/Bibi&James_0057.jpg",
+//   "../src/assets/Photography/David_Fernanda_4.jpg",
+//   "../src/assets/Photography/Denise&Adrian_0011.jpg",
+//   "../src/assets/Photography/David_Fernanda_9.jpg",
+//   "../src/assets/Photography/Denise&Adrian_0028.jpg",
+//   "../src/assets/Photography/David_Fernanda_6.jpg",
+//   "../src/assets/Photography/David_Fernanda_10.jpg",
+//   "../src/assets/Photography/Denise&Adrian_0026.jpg",
+//   "../src/assets/Photography/David_Fernanda_8.jpg",
 ];
 
 export default myPhotos;
