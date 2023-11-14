4fd1a241d9a2a7161c0bd482ad6cdac9e21aba8b
Sauljr144 <saulsuazojr06@gmail.com>
2023-11-14 12:57:14 -0800

    phototest8

--- portfolio/src/ThePhotos.js
+++ portfolio/src/ThePhotos.js
@@ -3,11 +3,11 @@ import p1 from "../src/assets/Photography/Bibi&James_0025.jpg"
 import p2 from "../src/assets/David_Fernanda_1.jpg"
 const myPhotos = [
     p1,
-    p2
+    p2,
 //   "../src/assets/Photography/Bibi&James_0025.jpg",
 //   "../src/assets/Photography/David_Fernanda_1.jpg",
 //   "../src/assets/Photography/Bibi&James_0038.jpg",
-//   "../src/assets/Photography/David_Fernanda_2.jpg",
+  "../src/assets/David_Fernanda_2.jpg",
 //   "../src/assets/Photography/Bibi&James_0032.jpg",
 //   "../src/assets/Photography/Bibi&James_0037.jpg",
 //   "../src/assets/Photography/Bibi&James_0047.jpg",
