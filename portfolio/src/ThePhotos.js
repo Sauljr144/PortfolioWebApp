e1b12cb60195aba98cf668732d8929cf801745b3
Sauljr144 <saulsuazojr06@gmail.com>
2023-11-14 15:35:06 -0800

    phototest9

--- portfolio/src/ThePhotos.js
+++ portfolio/src/ThePhotos.js
@@ -1,27 +1,41 @@
 
 import p1 from "../src/assets/Photography/Bibi&James_0025.jpg"
 import p2 from "../src/assets/David_Fernanda_1.jpg"
+import p3 from "../src/assets/Photography/Bibi&James_0038.jpg"
+import p4 from "../src/assets/David_Fernanda_2.jpg"
+import p5 from "../src/assets/Photography/Bibi&James_0032.jpg"
+import p6 from "../src/assets/Photography/Bibi&James_0037.jpg"
+import p7 from "../src/assets/Photography/Bibi&James_0047.jpg"
+import p8 from "../src/assets/David_Fernanda_3.jpg"
+import p9 from "../src/assets/Photography/Denise&Adrian_0007.jpg"
+import p10 from "../src/assets/Photography/Bibi&James_0057.jpg"
+import p11 from "../src/assets/David_Fernanda_4.jpg"
+import p12 from "../src/assets/Photography/Denise&Adrian_0011.jpg"
+import p13 from "../src/assets/David_Fernanda_9.jpg"
+import p14 from "../src/assets/Photography/Denise&Adrian_0028.jpg"
+import p15 from "../src/assets/David_Fernanda_6.jpg"
+import p16 from "../src/assets/David_Fernanda_10.jpg"
+import p17 from "../src/assets/Photography/Denise&Adrian_0026.jpg"
+import p18 from "../src/assets/David_Fernanda_8.jpg"
 const myPhotos = [
     p1,
     p2,
-//   "../src/assets/Photography/Bibi&James_0025.jpg",
-//   "../src/assets/Photography/David_Fernanda_1.jpg",
-//   "../src/assets/Photography/Bibi&James_0038.jpg",
-  "../src/assets/David_Fernanda_2.jpg",
-//   "../src/assets/Photography/Bibi&James_0032.jpg",
-//   "../src/assets/Photography/Bibi&James_0037.jpg",
-//   "../src/assets/Photography/Bibi&James_0047.jpg",
-//   "../src/assets/Photography/David_Fernanda_3.jpg",
-//   "../src/assets/Photography/Denise&Adrian_0007.jpg",
-//   "../src/assets/Photography/Bibi&James_0057.jpg",
-//   "../src/assets/Photography/David_Fernanda_4.jpg",
-//   "../src/assets/Photography/Denise&Adrian_0011.jpg",
-//   "../src/assets/Photography/David_Fernanda_9.jpg",
-//   "../src/assets/Photography/Denise&Adrian_0028.jpg",
-//   "../src/assets/Photography/David_Fernanda_6.jpg",
-//   "../src/assets/Photography/David_Fernanda_10.jpg",
-//   "../src/assets/Photography/Denise&Adrian_0026.jpg",
-//   "../src/assets/Photography/David_Fernanda_8.jpg",
+    p3,
+    p4,
+    p5,
+    p6,
+    p7,
+    p8,
+    p9,
+    p10,
+    p11,
+    p12,
+    p13,
+    p14,
+    p15,
+    p16,
+    p17,
+    p18,
 ];
 
 export default myPhotos;
