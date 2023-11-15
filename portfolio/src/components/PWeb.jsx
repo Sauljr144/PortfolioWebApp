0216c3adfa734d1c719053f29540e2d22b5f0e27
Sauljr144 <saulsuazojr06@gmail.com>
2023-11-14 20:49:32 -0800

    added links

--- portfolio/src/components/PWeb.jsx
+++ portfolio/src/components/PWeb.jsx
@@ -8,6 +8,9 @@ import {
   HStack,
   Image,
   Box,
+  Link,
+  LinkOverlay,
+  LinkBox,
 } from "@chakra-ui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -19,18 +22,17 @@ import {
   faHtml5,
   faCss3,
 } from "@fortawesome/free-brands-svg-icons";
-import typescirpt from "../assets/typescript.png"
-import csharp from "../assets/c_sharp.png"
-import weather from "../assets/WebProjects/WeatherApp.png"
-import top10 from "../assets/WebProjects/Top10.png"
-import flashCards from '../assets/WebProjects/FlashCards.png'
-import hangman from '../assets/WebProjects/hangman.png'
+import typescirpt from "../assets/typescript.png";
+import csharp from "../assets/c_sharp.png";
+import weather from "../assets/WebProjects/WeatherApp.png";
+import top10 from "../assets/WebProjects/Top10.png";
+import flashCards from "../assets/WebProjects/FlashCards.png";
+import hangman from "../assets/WebProjects/hangman.png";
 
 const PWeb = () => {
   return (
     <>
-
-<Container maxW="container.xl">
+      <Container maxW="container.xl">
         <Box
           mt={{ lg: "-125px", md: "-125px", base: "-40px" }}
           mb={10}
@@ -40,96 +42,123 @@ const PWeb = () => {
           alignItems="center"
           justifyContent="center"
           className="myNoise"
-          
         >
           <Text fontSize={30} fontWeight="bold" mt="50px">
             Projects
           </Text>
         </Box>
 
-      <Container
-        maxW="container.xl"
-        className="myNoise"
-        borderRadius={30}
-        p={{base: 30, md:35, lg:90}}
-      >
-        <Text fontSize={35} fontWeight={700} >
-          Web Projects
-        </Text>
-        <Text fontSize={18} fontWeight={700} mt={2} mb={2}>
-          What I’ve used for development:
-        </Text>
-        <HStack >
-          <FontAwesomeIcon icon={faGithub} size='lg' />
-          <FontAwesomeIcon icon={faReact} size="lg" />
-          <Image src={typescirpt} boxSize="19px" />
-          <FontAwesomeIcon icon={faJs} size="lg" />
-          <Image src={csharp} boxSize="20px" />
-          <FontAwesomeIcon icon={faUnity} size="lg" />
-          <FontAwesomeIcon icon={faBootstrap} size="lg" />
-          <FontAwesomeIcon icon={faHtml5} size="lg" />
-          <FontAwesomeIcon icon={faCss3} size="lg" />
-        </HStack>
+        <Container
+          maxW="container.xl"
+          className="myNoise"
+          borderRadius={30}
+          p={{ base: 30, md: 35, lg: 90 }}
+        >
+          <Text fontSize={35} fontWeight={700}>
+            Web Projects
+          </Text>
+          <Text fontSize={18} fontWeight={700} mt={2} mb={2}>
+            What I’ve used for development:
+          </Text>
+          <HStack>
+            <FontAwesomeIcon icon={faGithub} size="lg" />
+            <FontAwesomeIcon icon={faReact} size="lg" />
+            <Image src={typescirpt} boxSize="19px" />
+            <FontAwesomeIcon icon={faJs} size="lg" />
+            <Image src={csharp} boxSize="20px" />
+            <FontAwesomeIcon icon={faUnity} size="lg" />
+            <FontAwesomeIcon icon={faBootstrap} size="lg" />
+            <FontAwesomeIcon icon={faHtml5} size="lg" />
+            <FontAwesomeIcon icon={faCss3} size="lg" />
+          </HStack>
 
-        <Text textAlign="center" fontSize={22} fontWeight={700} mt={20} mb={2}>
-          My Top Favorite Projects I've Developed
-        </Text>
+          <Text
+            textAlign="center"
+            fontSize={22}
+            fontWeight={700}
+            mt={20}
+            mb={2}
+          >
+            My Top Favorite Projects I've Developed
+          </Text>
 
-        <SimpleGrid columns={{ sm: 1, md: 2 }} spacingX="30px" spacingY="30px">
-          <GridItem>
-            <Box
-              height="300px"
-              display="flex"
-              bgSize="cover"
-              bgPosition="center"
-              bgImage={weather}
-              borderRadius={20}
-            ></Box>
-            <Text mt={4} fontWeight={700} fontSize={20}>
-              Weather App
-            </Text>
-          </GridItem>
-          <GridItem>
-            <Box
-              height="300px"
-              display="flex"
-              bgSize="cover"
-              bgPosition="top"
-              bgImage={top10}
-              borderRadius={20}
-            ></Box>
-            <Text mt={4} fontWeight={700} fontSize={20}>
-              Top 10
-            </Text>
-          </GridItem>
-          <GridItem>
-            <Box
-              height="300px"
-              display="flex"
-              bgSize="cover"
-              bgPosition="center"
-              bgImage={flashCards}
-              borderRadius={20}
-            ></Box>
-            <Text mt={4} fontWeight={700} fontSize={20}>
-              Flash Cards
-            </Text>
-          </GridItem>
-          <GridItem>
-            <Box
-              height="300px"
-              display="flex"
-              bgSize="cover"
-              bgPosition='top'
-              bgImage={hangman}
-              borderRadius={20}
-            ></Box>
-            <Text mt={4} fontWeight={700} fontSize={20}>
-              Hangman
-            </Text>
-          </GridItem>
-        </SimpleGrid>
-      </Container>
+          <SimpleGrid
+            columns={{ sm: 1, md: 2 }}
+            spacingX="30px"
+            spacingY="30px"
+          >
+            <GridItem>
+              <LinkBox>
+                <Box
+                  height="300px"
+                  display="flex"
+                  bgSize="cover"
+                  bgPosition="top"
+                  bgImage={top10}
+                  borderRadius={20}
+                >
+                  <LinkOverlay href="" />
+                </Box>
+              </LinkBox>
+              <Text mt={4} fontWeight={700} fontSize={20}>
+                Top 10
+              </Text>
+            </GridItem>
+
+            <GridItem>
+              <LinkBox>
+                <Box
+                  height="300px"
+                  display="flex"
+                  bgSize="cover"
+                  bgPosition="center"
+                  bgImage={weather}
+                  borderRadius={20}
+                >
+                  <LinkOverlay href="https://black-plant-08e7aa81e.4.azurestaticapps.net/" />
+                </Box>
+              </LinkBox>
+              <Text mt={4} fontWeight={700} fontSize={20}>
+                Weather App
+              </Text>
+            </GridItem>
+
+            <GridItem>
+              <LinkBox>
+                <Box
+                  height="300px"
+                  display="flex"
+                  bgSize="cover"
+                  bgPosition="center"
+                  bgImage={flashCards}
+                  borderRadius={20}
+                >
+                  <LinkOverlay href="https://victorious-meadow-06551e51e.4.azurestaticapps.net/" />
+                </Box>
+              </LinkBox>
+              <Text mt={4} fontWeight={700} fontSize={20}>
+                Flash Cards
+              </Text>
+            </GridItem>
+            <GridItem>
+              <LinkBox>
+                <Box
+                  height="300px"
+                  display="flex"
+                  bgSize="cover"
+                  bgPosition="top"
+                  bgImage={hangman}
+                  borderRadius={20}
+                >
+                  <LinkOverlay href="https://kind-tree-0dc2f8a1e.4.azurestaticapps.net/" />
+                </Box>
+              </LinkBox>
+              <Text mt={4} fontWeight={700} fontSize={20}>
+                Hangman
+              </Text>
+            </GridItem>
+          </SimpleGrid>
+        </Container>
       </Container>
     </>
   );
