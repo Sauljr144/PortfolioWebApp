462761c546efcf8bb21daa6eccf4aba514a86636
Sauljr144 <saulsuazojr06@gmail.com>
2023-11-15 17:16:44 -0800

    404test1.5

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,6 +1,11 @@
 import "./App.css";
-import { BrowserRouter, Route, Routes, useLocation} from "react-router-dom";
-import { useEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Redirect,
+} from "react-router-dom";
+import { useEffect } from "react";
 import HomePage from "./components/HomePage";
 import AboutPage from "./components/AboutPage";
 import ProjectsPage from "./components/ProjectsPage";
@@ -12,40 +17,24 @@ import PFilm from "./components/PFilm";
 import PPhoto from "./components/PPhoto";
 import NotFound from "./components/NotFound";
 
-
 function App() {
-
-  const location = useLocation();
-
-  useEffect(() => {
-    if (location.pathname === '*') {
-      window.location.href = '/';
-    }
-  }, [location]);
-  
   return (
     <>
-   
-    
-    <BrowserRouter>
-   
-      <MyNavBar/>
-      <Routes>
-        
-        <Route path="/" element={<HomePage />} />
-        <Route path="/AboutPage" element={<AboutPage />} />
-        <Route path="/ProjectsPage" element={<ProjectsPage />} />
-        <Route path="/Web" element={<PWeb/>} />
-        <Route path="/Filmmaking" element={<PFilm/>} />
-        <Route path="/Photography" element={<PPhoto/>} />
-        <Route path="/Contact" element={<Contact />} />
-        
-  
-      
-      </Routes>
-     
-    <Footer/>
-    </BrowserRouter>
+      <Router>
+        <MyNavBar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/AboutPage" element={<AboutPage />} />
+          <Route path="/ProjectsPage" element={<ProjectsPage />} />
+          <Route path="/Web" element={<PWeb />} />
+          <Route path="/Filmmaking" element={<PFilm />} />
+          <Route path="/Photography" element={<PPhoto />} />
+          <Route path="/Contact" element={<Contact />} />
+          <Route path="*" element={<Redirect to="/" />} />
+        </Routes>
+
+        <Footer />
+      </Router>
     </>
   );
 }
